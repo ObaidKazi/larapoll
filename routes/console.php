fd7e83ac1f2a0dc394b832a2ab505fe8c7a51827
Obaid Kazi <Obaidkazi03@gmail.com>
2026-06-28 13:54:12 +0530

    fix the issue of cmd

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('votes:sync')->everyFiveMinutes();
+Schedule::command('app:sync-vote-counts')->everyFiveMinutes();
